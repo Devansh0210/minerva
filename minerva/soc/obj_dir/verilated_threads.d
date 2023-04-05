dbccdd145b3fa9e4ba5dc590339876fdfec1f0bf
Devansh Tanna <devansht0210@gmail.com>
2023-04-05 21:02:42 +0530

    Added utils to get IO ports

--- minerva/soc/obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /home/devector/Documents/verilator/include/verilated_threads.cpp \
- /home/devector/Documents/verilator/include/verilatedos.h \
- /home/devector/Documents/verilator/include/verilated_threads.h \
- /home/devector/Documents/verilator/include/verilated.h \
- /home/devector/Documents/verilator/include/verilated_config.h \
- /home/devector/Documents/verilator/include/verilated_types.h \
- /home/devector/Documents/verilator/include/verilated_funcs.h
--- minerva/soc/obj_dir/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
- /home/devector/Documents/verilator/include/verilated_timing.cpp \
- /home/devector/Documents/verilator/include/verilated_timing.h \
- /home/devector/Documents/verilator/include/verilated.h \
- /home/devector/Documents/verilator/include/verilatedos.h \
- /home/devector/Documents/verilator/include/verilated_config.h \
- /home/devector/Documents/verilator/include/verilated_types.h \
- /home/devector/Documents/verilator/include/verilated_funcs.h
